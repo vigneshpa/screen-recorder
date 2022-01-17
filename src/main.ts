48f05c70edb1cf99ebfd7fd7e5ef6d60a43d20cb
vignesh <vignesh-p@outlook.com>
2022-01-17 15:00:41 +0530

    including transform stream ponny fill

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import App from './App.svelte';
 import { registerSW } from 'virtual:pwa-register';
+import ponyfill from './ponyfill';
 
 registerSW({
   onNeedRefresh() {},
   onOfflineReady() {},
 });
+const mount = () => (window.app = new App({ target: window.document.body }));
 
-window.app = new App({ target: window.document.body });
+ponyfill().then(mount);
 
 declare global {
   interface Window {
